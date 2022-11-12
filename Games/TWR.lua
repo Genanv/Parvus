3d39044da6cf74d4de8b53a5d3c105ad7d78b8a9
Genanv <77456343+Genanv@users.noreply.github.com>
2022-11-11 19:36:12 -0800

    Update TWR.lua

--- Games/TWR.lua
+++ Games/TWR.lua
@@ -16,9 +16,9 @@ local RemoteEvent = ReplicatedStorage:WaitForChild("RE")
 local vim = game:GetService('VirtualInputManager')
 
 local function m1click() 
-    vim:SendMouseButtonEvent(545.5,307,1,true,game,0)
+    vim:SendMouseButtonEvent(545.5,307,0,true,game,0)
     wait()
-    vim:SendMouseButtonEvent(545.5,307,1,false,game,0)
+    vim:SendMouseButtonEvent(545.5,307,0,false,game,0)
 end
 
 local OCIFunction for Index,Function in pairs(getgc()) do
